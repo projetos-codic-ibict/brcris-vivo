d56c58dfab116f48684522fda0a52815afc9b298
Jesiel Viana <jesielviana@proton.me>
2024-10-23 07:01:24 -0300

    configurando script para deploy na máquina de dev do ibict

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
+set -e
+echo "Deploying VIVO Dev"
+
+echo "git pull"
 git pull
+
+echo "mvn clean package -s installer/example-settings.xml"
 mvn clean package -s installer/example-settings.xml
+
+echo "sudo rm -rf /opt/tomcat/webapps/vivo-dev"
 sudo rm -rf /opt/tomcat/webapps/vivo-dev
+
+echo "sudo cp -rf installer/webapp/target/vivo /opt/tomcat/webapps/vivo-dev"
 sudo cp -rf installer/webapp/target/vivo /opt/tomcat/webapps/vivo-dev
+
+echo "sudo systemctl restart tomcat.service"
 sudo systemctl restart tomcat.service
